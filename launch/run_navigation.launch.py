e4b3ae35d6d38180064b0cf7c89b9e74436a11e0
Naoki Takahashi <naoki.takahashi060@gmail.com>
2023-09-10 20:29:42 +0900

    lint

--- launch/run_navigation.launch.py
+++ launch/run_navigation.launch.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2023 NaokiTakahashi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
-import sys
-import glob
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import (
-    LaunchDescription,
-    LaunchService
-)
+from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
@@ -18,20 +31,14 @@ from launch.conditions import (
     IfCondition,
     UnlessCondition
 )
-from launch.launch_description_sources import (
-    AnyLaunchDescriptionSource,
-    PythonLaunchDescriptionSource
-)
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import (
-    Node,
-    PushRosNamespace
-)
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     sim_pkg_share_dir = get_package_share_directory('simply_diff_drive_simulator')
 
-    nav2_bringup_pkg = 'nav2_bringup'
     nav2_bringup_pkg_share_dir = get_package_share_directory('nav2_bringup')
 
     namespace = LaunchConfiguration('namespace')
@@ -41,33 +48,39 @@ def generate_launch_description():
     slam = LaunchConfiguration('slam')
 
     return LaunchDescription([
-        DeclareLaunchArgument('namespace',
-            default_value = [''],
-            description = 'Namespace of simply diff drive robot simulation'
+        DeclareLaunchArgument(
+            'namespace',
+            default_value=[''],
+            description='Namespace of simply diff drive robot simulation'
         ),
-        DeclareLaunchArgument('use_sim_time',
-            default_value = ['true'],
-            description = 'Enable time resource from /clock (true/false)'
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=['true'],
+            description='Enable time resource from /clock (true/false)'
         ),
-        DeclareLaunchArgument('autostart',
-            default_value = ['true'],
-            description = 'Enable autostart navigation2 lifecycle manager (true/false)'
+        DeclareLaunchArgument(
+            'autostart',
+            default_value=['true'],
+            description='Enable autostart navigation2 lifecycle manager (true/false)'
         ),
-        DeclareLaunchArgument('params_file',
-            default_value = os.path.join(
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=os.path.join(
                 sim_pkg_share_dir, 'config', 'nav2.yaml'
             ),
-            description = 'Navigation2 parameter file path (Full path)'
+            description='Navigation2 parameter file path (Full path)'
         ),
-        DeclareLaunchArgument('map',
-            default_value = os.path.join(
+        DeclareLaunchArgument(
+            'map',
+            default_value=os.path.join(
                 sim_pkg_share_dir, 'maps', 'simulator_map.yaml'
             ),
-            description = 'Map config file path for 2D map_server (Full path)'
+            description='Map config file path for 2D map_server (Full path)'
         ),
-        DeclareLaunchArgument('slam',
-            default_value = ['true'],
-            description = 'Enable sync slam by slam_toolbox (true/false)'
+        DeclareLaunchArgument(
+            'slam',
+            default_value=['true'],
+            description='Enable sync slam by slam_toolbox (true/false)'
         ),
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource(os.path.join(
@@ -75,7 +88,7 @@ def generate_launch_description():
                 'launch',
                 'navigation_launch.py'
             )),
-            launch_arguments = {
+            launch_arguments={
                 'namespace': namespace,
                 'use_sim_time': use_sim_time,
                 'autostart': autostart,
@@ -88,7 +101,7 @@ def generate_launch_description():
             AnyLaunchDescriptionSource(
                 os.path.join(nav2_bringup_pkg_share_dir, 'launch', 'localization_launch.py')
             ),
-            launch_arguments = {
+            launch_arguments={
                 'namespace': namespace,
                 'use_sim_time': use_sim_time,
                 'autostart': autostart,
@@ -96,56 +109,44 @@ def generate_launch_description():
                 'use_lifecycle_mgr': 'true',
                 'map': LaunchConfiguration('map')
             }.items(),
-            condition = UnlessCondition(slam)
+            condition=UnlessCondition(slam)
         ),
         GroupAction([
             Node(
-                package = 'slam_toolbox',
-                executable = 'async_slam_toolbox_node',
-                name = 'slam_toolbox',
-                output = 'screen',
-                parameters = [
+                package='slam_toolbox',
+                executable='async_slam_toolbox_node',
+                name='slam_toolbox',
+                output='screen',
+                parameters=[
                     os.path.join(
                         sim_pkg_share_dir, 'config', 'slam_toolbox.yaml'
                     ),
                     {'use_sim_time': use_sim_time}
                 ],
-                condition = IfCondition(slam)
+                condition=IfCondition(slam)
             ),
             Node(
-                package = 'nav2_map_server',
-                executable = 'map_saver_server',
-                name = 'map_saver',
-                output = 'screen',
-                parameters = [
+                package='nav2_map_server',
+                executable='map_saver_server',
+                name='map_saver',
+                output='screen',
+                parameters=[
                     params_file
                 ],
-                condition = IfCondition(slam)
+                condition=IfCondition(slam)
             ),
             Node(
-                package = 'nav2_lifecycle_manager',
-                executable = 'lifecycle_manager',
-                name = 'lifecycle_manager_slam',
-                output = 'screen',
-                parameters = [
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_slam',
+                output='screen',
+                parameters=[
                     params_file,
                     {'use_sim_time': use_sim_time},
                     {'autostart': autostart},
                     {'node_names': ['map_saver']}
                 ],
-                condition = IfCondition(slam)
+                condition=IfCondition(slam)
             )
         ])
-       #IncludeLaunchDescription(
-       #    PythonLaunchDescriptionSource(
-       #        os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
-       #    ),
-       #    launch_arguments = {
-       #        'namespace': '',
-       #        'use_sim_time': use_sim_time,
-       #        'map': LaunchConfiguration('map'),
-       #        'slam': LaunchConfiguration('slam'),
-       #        'params_file': LaunchConfiguration('params_file'),
-       #    }.items()
-       #),
     ])
--- launch/run_simulation.launch.py
+++ launch/run_simulation.launch.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2023 NaokiTakahashi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
 import sys
-import glob
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -11,23 +28,13 @@ from launch import (
 )
 from launch.actions import (
     DeclareLaunchArgument,
-    IncludeLaunchDescription,
-    GroupAction
+    IncludeLaunchDescription
 )
-from launch.conditions import (
-    IfCondition,
-    UnlessCondition
-)
-from launch.frontend import Parser
-from launch.launch_description_sources import (
-    AnyLaunchDescriptionSource,
-    PythonLaunchDescriptionSource
-)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import (
-    Node,
-    PushRosNamespace
-)
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     sim_pkg_share_dir = get_package_share_directory('simply_diff_drive_simulator')
@@ -36,33 +43,40 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     return LaunchDescription([
-        DeclareLaunchArgument('namespace',
-            default_value = ['diff_bot'],
-            description = 'Namespace of simply diff drive robot simulation (string)'
+        DeclareLaunchArgument(
+            'namespace',
+            default_value=['diff_bot'],
+            description='Namespace of simply diff drive robot simulation (string)'
         ),
-        DeclareLaunchArgument('output',
-            default_value = ['screen'],
-            description = 'Node output information'
+        DeclareLaunchArgument(
+            'output',
+            default_value=['screen'],
+            description='Node output information'
         ),
-        DeclareLaunchArgument('use_sim_time',
-            default_value = ['true'],
-            description = 'Enable time resource from /clock (true/false)'
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=['true'],
+            description='Enable time resource from /clock (true/false)'
         ),
-        DeclareLaunchArgument('no_ign_gui',
-            default_value = ['True'],
-            description = 'Disable ignition gazebo gui (True/False)'
+        DeclareLaunchArgument(
+            'no_ign_gui',
+            default_value=['True'],
+            description='Disable ignition gazebo gui (True/False)'
         ),
-        DeclareLaunchArgument('with_navigation',
-            default_value = ['true'],
-            description = 'Launch with run_navigation.launch.py (true/false)'
+        DeclareLaunchArgument(
+            'with_navigation',
+            default_value=['true'],
+            description='Launch with run_navigation.launch.py (true/false)'
         ),
-        DeclareLaunchArgument('slam',
-            default_value = ['true'],
-            description = 'Enable sync slam by slam_toolbox (true/false)'
+        DeclareLaunchArgument(
+            'slam',
+            default_value=['true'],
+            description='Enable sync slam by slam_toolbox (true/false)'
         ),
-        DeclareLaunchArgument('with_rviz',
-            default_value = ['true'],
-            description = 'Launch with visualize.launch.xml (true/false)'
+        DeclareLaunchArgument(
+            'with_rviz',
+            default_value=['true'],
+            description='Launch with visualize.launch.xml (true/false)'
         ),
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource([
@@ -72,18 +86,18 @@ def generate_launch_description():
                     'ignition_gazebo.launch.py'
                 )
             ]),
-            launch_arguments = {
+            launch_arguments={
                 'namespace': namespace,
                 'use_sim_time': use_sim_time
             }.items()
         ),
         Node(
-            package = 'robot_localization',
-            executable = 'ekf_node',
-            name = 'ekf_node',
-            namespace = namespace,
-            output = 'screen',
-            parameters = [
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_node',
+            namespace=namespace,
+            output='screen',
+            parameters=[
                 {os.path.join(sim_pkg_share_dir, 'config', 'ekf.yaml')},
                 {'use_sim_time': use_sim_time},
                 {'odom_frame': 'diff_bot/odom'},
@@ -91,7 +105,7 @@ def generate_launch_description():
                 {'odom0': 'odom/raw'},
                 {'imu0': 'imu/data'}
             ],
-            remappings = [
+            remappings=[
                ('odometry/filtered', 'odom/filtered')
             ]
         ),
@@ -103,13 +117,13 @@ def generate_launch_description():
                     'run_navigation.launch.py'
                 )
             ),
-            launch_arguments = {
+            launch_arguments={
                 'namespace': '',
                 'use_sim_time': use_sim_time,
                 'slam': LaunchConfiguration('slam'),
                 'params_file': os.path.join(sim_pkg_share_dir, 'config', 'nav2.yaml'),
             }.items(),
-            condition = IfCondition(LaunchConfiguration('with_navigation'))
+            condition=IfCondition(LaunchConfiguration('with_navigation'))
         ),
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource(
@@ -119,22 +133,23 @@ def generate_launch_description():
                     'visualize.launch.xml'
                 )
             ),
-            launch_arguments = {
+            launch_arguments={
                 'namespace': '',
                 'use_sim_time': use_sim_time,
             }.items(),
-            condition = IfCondition(LaunchConfiguration('with_rviz'))
+            condition=IfCondition(LaunchConfiguration('with_rviz'))
         )
     ])
 
-def main(argv = sys.argv[1:]):
+
+def main(argv=sys.argv[1:]):
     ld = generate_launch_description()
 
-    ls = LaunchService(argv = argv)
+    ls = LaunchService(argv=argv)
     ls.include_launch_description(ld)
 
     return ls.run()
 
+
 if __name__ == '__main__':
     sys.exit(main())
-
